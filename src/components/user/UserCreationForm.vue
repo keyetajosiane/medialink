cc67846834344ea0079b991ce108528e0be3b615
bayanga antoine <badinfo1987@gmail.com>
2023-12-26 17:09:16 +0100

    accounts creation

--- src/components/user/UserCreationForm.vue
+++ src/components/user/UserCreationForm.vue
@@ -1,9 +1,10 @@
 <template>
+    <vue-basic-alert ref="alert" :duration="1000" :closeIn="8000" />
     <div class="min-h-screen bg-gray-700 flex justify-center items-center">
         <form @submit.prevent="handleSubmit" class="bg-gray-800 p-8 rounded-lg space-y-4 w-full">
             <h1 class="text-2xl font-bold text-white leading-tight text-center">Account Creation</h1>
             <!-- Base Account Info -->
-            <BaseAccountInfo v-slot="{ accountInfo }"> 
+            <BaseAccountInfo v-slot="{ accountInfo }">
                 <!-- Conditionally rendered components based on selected role -->
                 <div class="flex flex-wrap justify-center gap-20 items-center">
                     <div class="max-w-md">
@@ -16,7 +17,7 @@
                         <FormButton class="mt-4 w-full" type="submit">
                             Create Account
                         </FormButton>
-        
+                        <FormIndicator v-if="loading" message="creating account ..." class="mt-4 text-white" />
                     </div>
                 </div>
             </BaseAccountInfo>
@@ -26,15 +27,33 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import BaseAccountInfo from './BaseAccountInfo.vue';
 import FormateurInfo from './FormateurInfo.vue';
 import ApprenantInfo from './ApprenantInfo.vue';
 import AdministratifInfo from './AdministratifInfo.vue';
 import FormButton from '../formFields/FormButton.vue';
+import FormIndicator from '../forms/FormIndicator.vue';
+import VueBasicAlert from 'vue-basic-alert';
+import { initFlowbite } from 'flowbite';
+
+const loading = ref(false);
+const alert = ref(null);
+
+onMounted(() => {
+    initFlowbite();
+});
 
 const handleSubmit = () => {
     // Implement submission logic here
+    // Simulate account creation
+    loading.value = true;
+
+    // Simulate a delay for account creation
+    setTimeout(() => {
+        loading.value = false;
+        alert.value.showAlert('success', 'Account creation successful', "success!!");
+    }, 2000);
 };
 </script>
 
